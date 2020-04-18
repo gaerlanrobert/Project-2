5c0c56f8af9d28396fe02826e0aae6490bfed270
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-04-17 19:31:07 -0700

    heroku push debugging

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,6 +22,14 @@ module.exports = (app) => {
         if (req.user) {
             res.redirect('/members');
         }
+        res.sendFile(path.join(__dirname, '../public/login.html'));
+    });
+
+    app.get('/signup', (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect('/members');
+        }
         res.sendFile(path.join(__dirname, '../public/signup.html'));
     });
 
